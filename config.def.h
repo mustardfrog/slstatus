9ad7150209d41c6506a36fa75717da0354a3aff6
Frog <frogdoctor@tutanota.com>
2022-04-14 22:23:54 +0630

    fixed indentation

--- config.def.h
+++ config.def.h
@@ -66,13 +66,13 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 
 	{ datetime, " %s",                      "%d %a %b %Y %T" },
-    //{ battery_perc, "[  %s%%",             "BAT0" },
-    //{ battery_state, "(%s)] ",              "BAT0" },
-	{ cpu_perc, " |CPU %s%% ",              NULL},
-    //{ cpu_freq, "%sHz] ",                   NULL},
+	{ cpu_perc, "|CPU %s%% ",              NULL},
 	{ ram_perc, "RAM %s%% "   ,             NULL},
 	{ swap_used, "SWAP %s|",                NULL},
     { disk_used, "Used %s",                 "/" },
-    { keymap, "| %s",                       NULL },
+    { keymap, "|%s",                       NULL },
+    //{ battery_perc, "[  %s%%",             "BAT0" },
+    //{ battery_state, "(%s)] ",              "BAT0" },
+    //{ cpu_freq, "%sHz] ",                   NULL},
 	//{ vol_perc, "VOL %s%% "   ,           "/usr/bin/pactl"},
 };
